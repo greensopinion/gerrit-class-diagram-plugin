f5f07eba2fe2ef08b3fdaa835a88484798bf08db
David Green <david.green@tasktop.com>
2018-04-23 13:32:23 -0700

    add stub Gerrit plug-in, reorganize project layout
    Change-Id: Ifc6c896afb95d5b03cbcf0872693fa28033671cf

---
+++ visualcodereview-gerrit-plugin/src/main/java/com/tasktop/codejam/visualcodereview/gerrit/HttpModule.java
@@ -0,0 +1,9 @@
+package com.tasktop.codejam.visualcodereview.gerrit;
+
+import com.google.gerrit.httpd.plugins.HttpPluginModule;
+
+public class HttpModule extends HttpPluginModule {
+	@Override
+	protected void configureServlets() {
+	}
+}
---
+++ visualcodereview-gerrit-plugin/src/main/java/com/tasktop/codejam/visualcodereview/gerrit/Module.java
@@ -0,0 +1,11 @@
+package com.tasktop.codejam.visualcodereview.gerrit;
+
+import com.google.inject.AbstractModule;
+
+public class Module extends AbstractModule {
+
+	@Override
+	protected void configure() {
+
+	}
+}
